1d4d80d3d3bfa0044b3e9a441fba1ea3a53ce8a0
David <00184217@uca.edu.sv>
2020-04-18 12:37:20 -0600

    listo, tarea clase 8

--- ejemplo/src/main/java/com/uca/capas/ejemplo/controller/MainController.java
+++ ejemplo/src/main/java/com/uca/capas/ejemplo/controller/MainController.java
@@ -15,15 +15,32 @@ public class MainController {
 		return "Nombre: David Guardado, carnet: 00184217, carrera: Ing. Informatica, AÑo cursado: 4to";
 	}
 	@RequestMapping("/fecha")
-	public @ResponseBody int fecha(HttpServletRequest request) {
+	public @ResponseBody String fecha(HttpServletRequest request) {
 		Calendar calendario = Calendar.getInstance();
 		String dia;
-		Integer diaf = Integer.parseInt(request.getParameter("dia"));
-		Integer mes = Integer.parseInt(request.getParameter("mes"));
+		Integer diaf = Integer.parseInt(request.getParameter("dia"))-1;
+		Integer mes = Integer.parseInt(request.getParameter("mes"))-1;
 		Integer anio = Integer.parseInt(request.getParameter("anio"));
 		calendario.set(anio, mes, diaf);
 		int comp= calendario.get(Calendar.DAY_OF_WEEK);
-	
-		return comp;
+		switch (comp) {
+		case 1: dia = "Lunes";
+		break;
+		case 2: dia = "Martes";
+		break;
+		case 3: dia = "Miercoles";
+		break;
+		case 4: dia = "Jueves";
+		break;
+		case 5: dia = "Viernes";
+		break;
+		case 6: dia = "Sabado";
+		break;
+		case 7: dia = "Domingo";
+		break;
+		default: dia = "Ningun dia seleccionado";
+		break;
+	}
+		return "Dia de la semana enviado: " + dia;
 	}
 }
